61b602eca06c323d9b8b0743eee521f3f5d8420c
Daniel Alves <daniel.carvalho.alves970@gmail.com>
2021-05-25 21:16:32 -0300

    adjustments to the layout of the new tab

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Version 1.0 content
 
 * This is the first version of project, contains...
+
 # Content
 
 * Initial Icon (https://www.flaticon.com/br/icone-gratis/armazenamento-na-nuvem_2921459?term=pdf&page=4&position=39&page=4&position=39&related_id=2921459&origin=search)
 * New Tab to setup objects that will be used
 * Permission set to manage PDF Generator
+* New object to configure objects
+* Scripts to generate and delete test data
 
 # Before deploy process
 
--- force-app/main/default/lwc/pdfGeneratorConfigObjects/pdfGeneratorConfigObjects.js
+++ force-app/main/default/lwc/pdfGeneratorConfigObjects/pdfGeneratorConfigObjects.js
@@ -1,18 +1,91 @@
 import { LightningElement } from 'lwc';
+import getRecords from '@salesforce/apex/ControllerSetupObjects.getAllRecords';
+import { NavigationMixin } from 'lightning/navigation';
+
+const actions = [
+    { label: 'Show details', name: 'show_details' },
+    { label: 'Delete', name: 'delete' },
+];
 
 const columns = [
-    { label: 'Label', fieldName: 'name' },
-    { label: 'Developer Name', fieldName: 'website', type: 'url' },
-    { label: 'Template', fieldName: 'phone', type: 'phone' },
-    { label: 'Last Modified', fieldName: 'amount', type: 'currency' },
-    { label: 'Created', fieldName: 'closeAt', type: 'date' },
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Object', fieldName: 'Object__c' },
+    { label: 'Template', fieldName: 'Template__c' },
+    { label: 'Last Modified Date', fieldName: 'LastModifiedDate', type: 'date' },
+    { label: 'Created Date', fieldName: 'CreatedDate', type: 'date' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
 ];
 
 export default class PdfGeneratorConfigObjects extends LightningElement {
     data = [];
     columns = columns;
 
-    clickNew(event){
-        
+    connectedCallback(){
+        this.getRecords();
     }
+
+    clickNew(event){
+
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case 'delete':
+                this.deleteRow(row);
+                break;
+            case 'show_details':
+                this.showRowDetails(row);
+                break;
+            default:
+        }
+    }
+
+    deleteRow(row) {
+        this[NavigationMixin.Navigate]({
+            type: "standard__component",
+            attributes: {
+                componentName: "c__NavigateToLWC"
+            },
+            state: {
+                c__propertyValue: '500'
+            }
+        });
+        const { id } = row;
+        const index = this.findRowIndexById(id);
+        if (index !== -1) {
+            this.data = this.data
+                .slice(0, index)
+                .concat(this.data.slice(index + 1));
+        }
+    }
+
+    showRowDetails(row) {
+        console.log('ROW ',row.URLObject);
+        window.location.href = row.URLObject;
+    }
+
+    getRecords(){
+        getRecords()
+        .then(result => {
+            if (result) {
+                result.forEach(element => {
+                    element.URLObject = 'https://' + window.location.host + '/' + element.Id;
+                });
+                this.data = result
+                console.log('DATA -> ',this.data);
+            }else{
+                console.log('ERROR!');
+                console.log('result: ',result);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        }); 
+    }
+
 }
